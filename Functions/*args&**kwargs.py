e3c47036c6caaf105fc1fa2890d7dae72d2f03a6
PrashanthHosamani <prashanthprashanth6753@gmail.com>
2025-07-13 15:40:02 +0530

    update

--- Functions/*args&**kwargs.py
+++ Functions/*args&**kwargs.py
@@ -1,9 +1,8 @@
 #These are special python keywords that are used to pass the variable length of arguments to a function
 
-# *args
-#allows us to pass a variable number of non-keyword arguments to a function
-
 def multiply(*args):
+    """ *args allows us to pass a variable number of non-keyword arguments to a function 
+    This function gives the product of any number of arguments """
     prod = 1
     
     for i in args:
@@ -17,3 +16,25 @@ print(multiply(1,2,3,4,5,6))
 
 #Behind the scene (when we write * python understands there will be variable number of inputs) 
 # Internally it will create a tuple then it adds up the items
+
+
+#Documentation of the code 
+
+print(multiply.__doc__)
+print('\n')
+
+# **kwargs
+# **kwargs allows us to pass any number of keyword arguments.
+# keyword arguments mean that they contain a key-value pair, like a python dictionary.
+
+def display(**kwargs):
+    
+        
+    for (key, value) in kwargs.items():
+        print(key, "->", value)
+        
+display(india='delhi', srilanka = 'colombo', nepal = 'kathmandu')
+    
+
+        
+        
