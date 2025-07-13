ca348bdfdd983b2fcb35b999589527cdfedb28db
PrashanthHosamani <prashanthprashanth6753@gmail.com>
2025-07-14 00:27:38 +0530

    update

---
+++ Functions/NestedFunction.py
@@ -0,0 +1,22 @@
+#Function inside the function which is called the nested functions
+
+def f():
+    def g():
+        print('inside function g')
+    g()
+    print('inside function f')
+    
+f() #if we call the outside function than only we can access the inside function
+
+
+def g(x):
+    def h():
+        x = 'abc'
+    x = x + 1
+    print('in g(X): x =',  x)
+    h()
+    return x
+x = 3
+g(x)
+
+
--- Starting Project's/Timecom.py
+++
@@ -1,7 +0,0 @@
-def calculate_factorial(n):  # Function definition (calculate factorial)  
-    if n == 0 :              # Base case when 'n = 0', since the factoria         
-        return 1              
-    else :                    # Recursive Case, multiply "number" with                  
-       return n * calculate_factorial (n-1)          
-    
-print("The factorial of 5 is:",calculate_factorial(5))   # Call function 
