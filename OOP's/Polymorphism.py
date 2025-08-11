9fcb5fd12b8460294aaef931606d36644d1f5190
PrashanthHosamani <prashanthprashanth6753@gmail.com>
2025-08-11 20:49:21 +0530

    update

---
+++ OOP's/Polymorphism.py
@@ -0,0 +1,10 @@
+#The ability of an function or a method to act differently depending on the object it is acting upon 
+#having more faces depending on the situation
+#There are 3 concepts 1. Method Overriding, 2. Method Overloading, 3. Operator Overloading
+
+#Method Overriding -> already discussed 
+
+#Method Overloading --> Its a senario where in a single class having 2 methods with same name having different behaviour on the basis of input
+#But it does not work in python
+
+#Operator Overloading --> 
