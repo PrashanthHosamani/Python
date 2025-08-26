6705b0afc3cfca344805827cc2a8f00c7163cbf4
PrashanthHosamani <prashanthprashanth6753@gmail.com>
2025-08-26 22:57:15 +0530

    update

---
+++ Decorators/Decorators.py
@@ -0,0 +1,29 @@
+#A decorators in python is a function that receives another function as input and adds some functionality(decoration) to and it and returns it
+#This can happen only because python functions are 1st class citizens
+#There are 2 types of decorators available in python
+
+#Python are 1st class citizen
+
+def modify2(func, num):
+    return func(num)
+
+def square(num):
+    return num**2
+
+modify2(square, 2)
+
+
+#simple example 1
+
+def my_decorator(func):
+    def wrapper():
+        print('******************')
+        func()
+        print('************************')
+    return wrapper
+
+def hello():
+    print('hello')
+    
+a = my_decorator(hello)
+a()
---
+++ NameSpaces/Namespace.py
@@ -0,0 +1,13 @@
+#A namespace is a space that holds names(identifiers) programmatically speaking, namespace are dictionary of identifiers(keys) and their objects(values)
+
+""" 
+There are 4 types of namespaces:
+1. builtin Namespaces
+2. Global Namespaces
+3. Enclosing Namespaces
+4. Local Namespaces  
+
+"""
+#Scope and LEGB Rule
+
+ 
